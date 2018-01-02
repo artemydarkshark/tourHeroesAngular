cc59d3adfc57d417665cd3b78c4e01816c63490f
Maxim <max.darkshark@gmail.com>
2018-01-02 11:24:25 +0200

    hero full app

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+import { HeroService } from '../hero.service';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  constructor(private heroService: HeroService) { }
+
+  heroes: Hero[];
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) return;
+    this.heroService.addHero({ name } as Hero)
+      .subscribe( hero => {
+        this.heroes.push(hero);
+      })
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
+
+  getHeroes():void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);;;
+  }
+
+  ngOnInit() {
+    this.getHeroes()
+  }
+
+}
